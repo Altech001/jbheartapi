fce2b5811e0ee0a8b97575fc20e976ab889b2e8f
Abaasa Albert <164334496+Altech001@users.noreply.github.com>
2025-07-07 17:05:45 +0300

    update for cros main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from routes.books import book_router
 from routes.bookform import bookform_router
 from routes.videos import video_router
 
+from fastapi.middleware.cors import CORSMiddleware
 
 
 import schema
@@ -13,6 +14,14 @@ from database import Base, engine, get_db
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # List of allowed origins
+    allow_credentials=True,  # Allow cookies and credentials
+    allow_methods=["*"],     # Allow all HTTP methods (GET, POST, PUT, etc.)
+    allow_headers=["*"],     # Allow all headers
+)
 
 app.include_router(photo_router)
 app.include_router(book_router)
